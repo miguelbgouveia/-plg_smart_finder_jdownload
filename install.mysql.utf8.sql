1104389465f0b155789e867fab805752e688c517
unknown <robertocaires@TAG000028054.grm.gov-madeira.pt>
2016-02-26 17:28:59 +0000

    Com a desinstalação a funcionar corretamente

--- install.mysql.utf8.sql
+++ install.mysql.utf8.sql
@@ -1,5 +1,5 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-ALTER TABLE `pagesp_jdownloads_files` ADD `id` INT(11) NOT NULL ;
-UPDATE pagesp_jdownloads_files SET id=file_id;
-CREATE TRIGGER `codigo` BEFORE UPDATE ON `pagesp_jdownloads_files` FOR EACH ROW SET new.id = new.file_id;
+ALTER TABLE `#__jdownloads_files` ADD `id` INT(11) NOT NULL ;
+UPDATE #__jdownloads_files SET id=file_id;
+CREATE TRIGGER `codigo` BEFORE UPDATE ON `#__jdownloads_files` FOR EACH ROW SET new.id = new.file_id;
 
---
+++ sqluninstall.mysql.utf8.sql
@@ -0,0 +1,3 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+ALTER TABLE `#__jdownloads_files` DROP `id`;
+DROP TRIGGER IF EXISTS `codigo`;
